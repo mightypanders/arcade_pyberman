227fceda66770ef41ab0aea62596fef3d4bc4043
mightypanders <mighty.panders@gmail.com>
2016-09-02 10:17:41 +0200

    keine ahnung

--- Game.py
+++ Game.py
@@ -5,4 +5,13 @@ import arcade
 class Game(States):
 	def __init__(self):
 		States.__init__(self)
+		self.next= "menu"
+		self.all_sprites = arcade.SpriteList()
+		self.wall_list = arcade.SpriteList()
+
+		self.countHorBlock = 8
+		self.countVertBlock = 10
+
+
+
 
